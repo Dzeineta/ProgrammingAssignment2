77fd1a70527f08747e5b96abf968db0136be13b7
Dzeineta <dzeineta.vailionyte@gmail.com>
2020-10-24 13:39:11 +0300

    Final file of cachematrix

--- cachematrix.R
+++ cachematrix.R
@@ -2,7 +2,7 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+## This function creates a special matrix object that can cache its inverse
 makeCacheMatrix <- function(x = matrix()) {
   i <- NULL
 ## Set the value of the matrix
